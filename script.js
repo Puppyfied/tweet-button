a9cb88d204128ec132e34ba600c58806e1796bdb
Glitch (glitch-hello-website) <none>
2022-04-25 17:03:39 +0000

    ⛑🐷 Checkpoint ./index.html:43085770/51 ./script.js:43085770/9 ./style.css:43085770/83

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var tweetBtn = document.getElementsByClassName("twtBtn");
+var tweetBtn = document.getElementsByClassName("tweetBtn");
 
 document.addEventListener("click", alertWhyTwt)
 
 function alertWhyTwt(){
   alert("imagine tweeting");
+  
 }
