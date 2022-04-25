a55b3f1d8df0f1b447cfca39d23731ec8772cda9
Glitch (glitch-hello-website) <none>
2022-04-25 17:15:37 +0000

    🚝⚓️ Checkpoint ./style.css:43085770/20 ./index.html:43085770/22 ./script.js:43085770/277

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var tweetBtn = document.getElementsByClassName("tweetBtn");
 
 document.addEventListener("click", alertWhyTwt)
-
+// alerts when tweet button is 
 function alertWhyTwt(){
   alert("imagine tweeting");
   
